a836160cc1d010374f5c1e73645ffc162cdf4223
Kuria <012nkuria@gmail.com>
2019-01-06 00:20:50 +0300

    Restore to its former glory

--- ViewXtended/Classes/KeyboardLayoutConstraintX.swift
+++ ViewXtended/Classes/KeyboardLayoutConstraintX.swift
@@ -1,54 +1,51 @@
 import UIKit
 
 public class KeyboardLayoutConstraint: NSLayoutConstraint {
-
+    
     /// A value that stores the constant at the time of initialization. Lazy so as to not access constant prematurely
     lazy var originalConstant = constant
     /// The current height of the keyboard, 0 if hidden
     open var keyboardVisibleHeight: CGFloat = 0
-
-    var keyBoardMaxHeight: CGFloat = 0
-
+    
     /// An additional offset experienced when the keyboard is visible
     /// If positive, the first item is pushed even further up
     @IBInspectable open var customInset: CGFloat = 0
-
+    
     override public init() {
         super.init()
         setup()
     }
-
+    
     func setup() {
-
+        
         NotificationCenter.default.addObserver(self, selector: #selector(KeyboardLayoutConstraint.keyboardWillShowNotification(_:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(KeyboardLayoutConstraint.keyboardWillHideNotification(_:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
-
+        
     }
-
+    
     override open func awakeFromNib() {
         super.awakeFromNib()
         setup()
     }
-
+    
     deinit {
         NotificationCenter.default.removeObserver(self)
     }
-
+    
     // MARK: Notification
-
+    
     @objc func keyboardWillShowNotification(_ notification: Foundation.Notification) {
         if let userInfo = notification.userInfo {
             if let frameValue = userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue {
                 let frame = frameValue.cgRectValue
-                keyBoardMaxHeight = frame.size.height
                 keyboardVisibleHeight = frame.size.height + customInset
             }
-
+            
             self.updateConstant()
             if let duration = userInfo[UIKeyboardAnimationDurationUserInfoKey] as? NSNumber,
                 let curve = userInfo[UIKeyboardAnimationCurveUserInfoKey] as? NSNumber {
                 let options = UIViewAnimationOptions(rawValue: curve.uintValue)
-
+                
                 UIView.animate(
                     withDuration: TimeInterval(duration.doubleValue),
                     delay: 0,
@@ -60,22 +57,19 @@ public class KeyboardLayoutConstraint: NSLayoutConstraint {
             }
         }
     }
-
+    
     @objc func keyboardWillHideNotification(_ notification: Foundation.Notification) {
-
+        keyboardVisibleHeight = 0
+        self.updateConstant()
+        
         if let userInfo = notification.userInfo {
-            if let frameValue = userInfo[UIKeyboardFrameBeginUserInfoKey] as? NSValue {
-                let frame = frameValue.cgRectValue
-                keyboardVisibleHeight = frame.size.height + customInset
-            }
-            self.updateConstant()
-
+            
             if let duration = userInfo[UIKeyboardAnimationDurationUserInfoKey] as? NSNumber,
                 let curve = userInfo[UIKeyboardAnimationCurveUserInfoKey] as? NSNumber {
-
-
+                
+                
                 let options = UIViewAnimationOptions(rawValue: curve.uintValue)
-
+                
                 UIView.animate(
                     withDuration: TimeInterval(duration.doubleValue),
                     delay: 0,
@@ -87,16 +81,16 @@ public class KeyboardLayoutConstraint: NSLayoutConstraint {
             }
         }
     }
-
+    
     open func updateConstant() {
         self.constant = originalConstant + keyboardVisibleHeight
     }
 }
 
 public class KeyboardLayoutConstraintX: KeyboardLayoutConstraint {
-
+    
     @IBInspectable open var inverted: Bool = false
-
+    
     override public func updateConstant() {
         if inverted {
             self.constant = originalConstant - keyboardVisibleHeight
